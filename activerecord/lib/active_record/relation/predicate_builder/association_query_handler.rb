650987893151b2c9b47a26374f9c88a50839da39
Kevin Hughes <kevinhughes27@gmail.com>
2019-07-08 13:38:14 -0400

    one additional required change due to other refactors that occured before PR #32148 in Rails

--- activerecord/lib/active_record/relation/predicate_builder/association_query_handler.rb
+++ activerecord/lib/active_record/relation/predicate_builder/association_query_handler.rb
@@ -20,7 +20,7 @@ module ActiveRecord
 
         table = value.associated_table
         if value.base_class
-          queries[table.association_foreign_type.to_s] = value.base_class.name
+          queries[table.association_foreign_type.to_s] = value.base_class.polymorphic_name
         end
 
         queries[table.association_foreign_key.to_s] = value.ids
